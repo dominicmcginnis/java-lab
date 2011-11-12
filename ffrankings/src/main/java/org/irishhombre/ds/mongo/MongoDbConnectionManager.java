86c015dddf3394f3db5c69de09003dbed77557a4
Dominic McGinnis <dominicmcginnis@gmail.com>
2011-11-11 16:21:16 -0800

    Adding in mongo db connection

---
+++ ffrankings/src/main/java/org/irishhombre/ds/mongo/MongoDbConnectionManager.java
@@ -0,0 +1,19 @@
+import com.mongodb.Mongo;
+
+public class MongoDbConnectionManager {
+  public static final MongoDbConnectionManager instance = new MongoDbConnectionManager();
+
+  private MongoDbConnectionManager() {
+	try {
+	  //TODO: Change this to a JVM property
+	  Mongo m = new Mongo("localhost", 27017);
+	} catch (Exception e) {
+	  throw new RuntimeException("Failed to initialize mongo....");
+	}
+  }
+
+  public static MongoDbConnectionManager getInstance() {
+	return instance;
+  }
+}
+
